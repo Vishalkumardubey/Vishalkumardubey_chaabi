f1f2cedfa52496ea999bddf75c2e2469bc8339e5
Vishalkumardubey <91932141+Vishalkumardubey@users.noreply.github.com>
2023-06-10 02:11:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import TypingPractice  from './components/TypingPractice';
+const App = () => {
+  return (
+    <div className="App">
+      <TypingPractice />
+    </div>
+  );
+};
+export default App;
